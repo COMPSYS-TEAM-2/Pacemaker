7c8edada7fa5714c7738c73b485736888fed2fcf
fosu562 <100399572+fosu562@users.noreply.github.com>
2023-09-18 11:00:45 +1200

    feat Added more test cases and updated the reader

--- testRunner.py
+++ testRunner.py
@@ -2,46 +2,43 @@ import csv
 
 
 def main():
-    file = open('test-0.csv', 'r+')
+    file = open('test-1.csv', 'r+')
 
     # Define all of the timers
     atov = 0
     vtoa = 0
     atoa = 0
+    time = 0
 
     atovCounts = []
     vtoaCounts = []
     atoaCounts = []
 
     # Define variables to count state
-    waitingA = True
-    waitingV = False
 
     csvReader = csv.DictReader(file)
 
     for col in csvReader:
+        time += 1
         # Increment the timers
         atov += 1
         vtoa += 1
         atoa += 1
-        if (waitingA):
-            # If there is an atrium event
-            if (int((col[' A']).strip()) == 1):
-                waitingV = True
-                atov = 0
-                # First two second can be ignored
-                if (float(col['Time']) > 2000):
-                    vtoaCounts.append(vtoa)
-                    atoaCounts.append(atoa)
-                atoa = 0
+        # If there is an atrium event
+        if (int((col[' A']).strip()) == 1 or int((col[' A']).strip()) == -1):
+            # First two second can be ignored
+            if (float(col['Time']) > 2000):
+                vtoaCounts.append(vtoa)
+                atoaCounts.append(atoa)
+            atov = 0
+            atoa = 0
 
-        if (waitingV):
-            if (int((col[' V']).strip()) == 1):
-                waitingA = True
-                vtoa = 0
-                # First two second can be ignored
-                if (float(col['Time']) > 2000):
-                    atovCounts.append(atov)
+        if (int((col[' V']).strip()) == 1 or int((col[' V']).strip()) == -1):
+            # First two second can be ignored
+            if (float(col['Time']) > 2000):
+                atovCounts.append(atov)
+            vtoa = 0
+
 
     print("A to V counts", atovCounts)
     print("V to A counts", vtoaCounts)
@@ -50,17 +47,22 @@ def main():
     aveAtoV = 0
     aveVtoA = 0
     aveAtoA = 0
-    for i in range(len(atoaCounts)):
-        aveAtoV += atovCounts[i]
-        aveVtoA += vtoaCounts[i]
-        aveAtoA += atoaCounts[i]
+    try:
+        for i in range(len(vtoaCounts)):
+            aveAtoV += atovCounts[i]
+            aveVtoA += vtoaCounts[i]
+            aveAtoA += atoaCounts[i]
+    except: 
+        print("i out of range")
 
-    aveAtoV /= i
-    aveVtoA /= i
-    aveAtoA /= i * 10
+    aveAtoV /= len(atoaCounts)
+    aveVtoA /= len(atoaCounts)
+    aveAtoA /= len(atoaCounts) 
+    bpm = (1/(aveAtoA / 1000)) * 60
     print("A to V = ", aveAtoV)
     print("V to A = ", aveVtoA)
-    print("BPM = ", aveAtoA)
+    print("A to A = ", aveAtoA)
+    print("BPM = ", bpm)
 
 
 if __name__ == '__main__':
