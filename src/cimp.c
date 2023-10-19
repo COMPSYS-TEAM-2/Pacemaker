537f5dcec7a8e99bf9a8c6ff2bd5c96b2f9aab6c
Franklin <fosu562@aucklanduni.ac.nz>
2023-10-19 14:28:23 +1300

    refactor Remove comments

--- src/cimp.c
+++ src/cimp.c
@@ -17,24 +17,19 @@ void c_logic(CData* d){
 	if (state == ATRIUM) {
 		// Transitions
 		if ((ventricleT >= PVARP_VALUE) && d->AS){
-//			printf("a");
 			setupVS();
 		} else if (ventricleT >= AEI_VALUE) {
-//			printf("b");
 			d->AP = 1;
 			setupVS();
 		}
 	} else if (state == VENTRICLE) {
 		// Transitions
 		if ((ventricleT >= VRP_VALUE) && d->VS){
-//			printf("a");
 			setupAS();
 		} else if ((ventricleT >= URI_VALUE) && (atriumT >= AVI_VALUE)){
-//			printf("b");
 			d->VP = 1;
 			setupAS();
 		} else if (ventricleT >= LRI_VALUE) {
-//			printf("c");
 			d->VP = 1;
 			setupAS();
 		}
@@ -44,13 +39,11 @@ void c_logic(CData* d){
 }
 
 void setupVS(){
-//	printf("AT: %f, VT: %f\n", atriumT, ventricleT);
 	atriumT = 0;
 	state = VENTRICLE;
 }
 
 void setupAS(){
-//	printf("AT: %f, VT: %f\n", atriumT, ventricleT);
 	ventricleT = 0;
 	state = ATRIUM;
 }
